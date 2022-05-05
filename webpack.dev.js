7d965734fb46d6accb50545d9974531ca359531d
Evgenii <iqoffee@gmail.com>
2022-05-05 22:28:45 +0500

    fix: add style loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -13,6 +13,7 @@ module.exports = merge(common, {
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
+          { loader: 'style-loader' },
           { loader: 'css-loader', options: { sourceMap: true } },
           { loader: 'postcss-loader', options: { sourceMap: true } },
           { loader: 'sass-loader', options: { sourceMap: true } },
