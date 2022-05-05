cebf3cdd010b5768c9c40d1650e870b2d67aa5d7
Evgenii <iqoffee@gmail.com>
2022-05-05 23:21:00 +0500

    feat: aadd class to keyboard

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import Keyboard from './Keyboard';
 const body = document.querySelector('body');
 const keyboard = new Keyboard();
 const keyboardBody = document.createElement('div');
+keyboardBody.classList.add('keyboard');
 
 let lang = 'en';
 
